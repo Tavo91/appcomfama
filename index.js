035e0abb358d21264becf0d911b46d277c7669d9
Tavo91 <geramirez01@gmail.com>
2023-06-27 11:40:13 -0500

    express

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const app = express();
 const apiRouter = require('./server')
 const cors = require("cors");
 
 
-const app = express();
 
 const port = process.env.PORT || 3000;
 
